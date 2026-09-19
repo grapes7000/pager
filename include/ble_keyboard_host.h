7da60f39b7269568ff8a024d5d9a0b46a67379ba
grapes7000 <grapes7000@proton.me>
2026-09-19 04:12:22 -0700

    Adopt EspBle Classic keyboard host

--- include/ble_keyboard_host.h
+++ include/ble_keyboard_host.h
@@ -1,8 +1,7 @@
 #pragma once
 
 #include <Arduino.h>
-#include <esp_gap_bt_api.h>
-#include <esp_hidh_api.h>
+#include <EspBleClassic.h>
 
 #include "keyboard_input.h"
 
@@ -19,27 +18,17 @@ class BleKeyboardHost : public KeyboardInput {
   InputEvent queue_[kQueueSize];
   volatile uint8_t head_ = 0;
   volatile uint8_t tail_ = 0;
-
   volatile bool initialized_ = false;
-  volatile bool hidReady_ = false;
   volatile bool connected_ = false;
   volatile bool scanning_ = false;
   volatile bool connecting_ = false;
-  volatile bool targetFound_ = false;
-
-  esp_bd_addr_t targetBda_ = {0};
-  uint8_t previous_[6] = {0};
+  String targetAddress_;
   uint32_t nextScanMs_ = 0;
+  EspBleClassic bluetooth_;
 
   void startInquiry();
-  void handleGapEvent(esp_bt_gap_cb_event_t event, esp_bt_gap_cb_param_t* param);
-  void handleHidEvent(esp_hidh_cb_event_t event, esp_hidh_cb_param_t* param);
-  void handleReport(const uint8_t* data, size_t length, uint16_t reportId);
   void push(InputEvent event);
-
   static bool looksLikeKeyboard(const String& name);
-  static char usageToAscii(uint8_t usage, bool shift);
-  static void gapCallback(esp_bt_gap_cb_event_t event, esp_bt_gap_cb_param_t* param);
-  static void hidCallback(esp_hidh_cb_event_t event, esp_hidh_cb_param_t* param);
+  static InputKey usageToKey(uint8_t usage, uint8_t ascii);
   static BleKeyboardHost* instance_;
 };
